9a6eb790c4b0c96863bd6ae1269d8d3960d31659
Ashot1990 <135628616+Ashot1990@users.noreply.github.com>
2023-06-29 13:26:08 +0400

    update lesson6

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
-import { useState, useMemo, useCallback } from "react"; // hook
-import Result from "./Result";
+import Main from "./components/Main/Main";
+import usersData from "./data/users";
+
+localStorage.setItem("users", JSON.stringify(usersData));
 
 function App() {
-  console.log("Render from App");
-  const [number, setNumber] = useState(0);
-  const [number2, setNumber2] = useState(0);
-  const [number3, setNumber3] = useState(0);
-  const [isShown, setIsShown] = useState(true);
-
-  const addPoint = useCallback(() => {
-    setNumber(number + 1);
-  }, [number]);
-  const addPoint2 = useCallback(() => {
-    setNumber2(number2 + 1);
-  }, [number2]);
-  const addPoint3 = useCallback(() => {
-    setNumber3(number3 + 1);
-  }, [number3]);
-  const result = useMemo(() => {
-    return number + number2;
-  }, [number, number2]);
-
-  const toggleResult = () => {
-    setIsShown(!isShown);
-  };
-
-  return (
-    <div>
-      <button onClick={addPoint}>Add +</button>
-      <button onClick={addPoint2}>Add2 +</button>
-      <button onClick={addPoint3}>Add3 +</button>
-      <button onClick={toggleResult}>Toggle</button>
-
-      <br />
-      <br />
-
-      <div>App Component - {number}</div>
-      <div>{number2}</div>
-      <div>{number3}</div>
-
-      <br />
-
-      <div>{result}</div>
-
-      {isShown && <Result number={number} />}
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
